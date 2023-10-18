8fac95a45cdaec9ab95db3915db91eec818f1154
G L E B U S <glebvollodin@gmail.com>
2023-10-18 17:01:52 +0300

    Practic2

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// 1 task
-let func = (age="18", name) => `My name is ${name}, I am ${age}.`;
-alert(func("Glebus"));
-
 // 2 task
+let func = (age="18", name) => `My name is ${name}, I am ${age}.`;
+// alert(func("Glebus"));
+
+// 3 task
 class Human {
     constructor(name, age, weight, gender) {
         this.name = name;
@@ -10,6 +10,23 @@ class Human {
         this.weight = weight;
         this.gender = gender;
     }
+    
+    getInfo(prop) {
+        if (prop === null) {
+            let info = `Name: ${this.name}\nAge: ${this.age}\nWeight: ${this.weight}\nGender: ${this.gender}`;
+            return info;
+        } else if (Object.keys(this).includes(prop)) {
+            return this[prop];
+        } else {
+            return `Invalid property: ${prop}`;
+        }
+    }
+
+    greeting() {
+        return "Hi!";
+    }
 }
 
 const person = new Human("John", 30, 70, "Male");
+
+alert(person.getInfo('age'));
